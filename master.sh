3037c891e3f59578bed747f3cc5264ca348b31da
Exanoff12 <91356410+Exanoff12@users.noreply.github.com>
2023-06-22 20:50:45 +0600

    Update master.sh

--- master.sh
+++ master.sh
@@ -3,10 +3,13 @@
 echo "Flash Checker v0.1 beta"
 echo "copyright - 2023 - Erik (AKPR)"
 
-if [ ! $1 ] then
+if [ ! $1 ]; then
 echo "usage: ./fc <mounted drive path>"
+else
+master_class
 fi
 
+master_class {
 KBreportedSize=$(df -k $1 | tail -1 | awk '{print $4}')
 reportedSize=$(($KBreportedSize * 1024))
 maxAllowedSize=1099511627776
@@ -14,5 +17,6 @@ maxAllowedSize=1099511627776
 if (( $reportedSize >= $maxAllowedSize )); then
 echo "drive reported space is larger than allowed size. aborting test"
 else
-func
+echo "continue"
 fi
+}
