b186d9085d6a4c2e958891d6f9648c7a6754db71
Exanoff12 <91356410+Exanoff12@users.noreply.github.com>
2023-06-22 20:43:58 +0600

    Update master.sh

--- master.sh
+++ master.sh
@@ -9,3 +9,10 @@ fi
 
 KBreportedSize=$(df -k $1 | tail -1 | awk '{print $4}')
 reportedSize=$(($KBreportedSize * 1024))
+maxAllowedSize=1099511627776
+
+if (( $reportedSize >= $maxAllowedSize )); then
+echo "drive reported space is larger than allowed size. aborting test"
+else
+func
+fi
